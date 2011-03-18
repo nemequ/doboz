29242c1c6accdeeb304881dd58693b9d8ea91e1c
attila_afra <unknown>
2011-03-18 16:58:49 +0200

    Additional include fixes

--- Source/Test/Test.cpp
+++ Source/Test/Test.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <algorithm>
 #include "Doboz/Compressor.h"
--- Source/Utils/FastRng.h
+++ Source/Utils/FastRng.h
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#include <cstdint>
+#include <stdint.h>
 
 namespace afra {
 
